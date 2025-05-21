31c91b8fbbd2d7eaf3a8d7dc91db72f24d777a52
isa <108779221+altierisa@users.noreply.github.com>
2025-05-21 15:45:59 -0300

    atividade_somatoria

---
+++ atividade_somatoria.py
@@ -0,0 +1,19 @@
+peso = float(input('Digite o seu peso: '))
+altura = float(input('Digite a sua altura: '))
+
+
+imc = peso / altura ** 2
+
+
+if imc <= 18.5:
+    print(f' IMC {imc} indica peso abaixo do normal.⚠️')
+elif 18.5 < imc <= 24.9:
+    print(f' IMC {imc} indica Eutrofia.✅')
+elif 24.9 < imc <= 29.9:
+    print(f' IMC {imc} indica Sobrepeso.⚠️')
+elif 29.9 < imc <= 34.9:
+    print(f' IMC {imc} indica Obesidadde grau I.🚨')
+elif 34.9 < imc <= 39.9:
+    print(f' IMC {imc} indica Obesidade grau II.🚨')
+else:
+    print(f' IMC {imc} indica Obesidade grau III (MÓRBIDA)🚨.')
